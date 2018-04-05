ec04bebac3c908c179eeea44dcf1b4e0c67ff7c0
jackschu <js9pa@virginia.edu>
2018-04-05 01:46:47 -0400

    removed debug lines

--- missinggnomes.cpp
+++ missinggnomes.cpp
@@ -4,36 +4,37 @@
 #include <unordered_set>
 using namespace std;
 int main(){
-  int x,y;
+  int x; // number of explicit / remaining gnomes
+  int y; // number of total gnomes
   while(cin >> y >> x){
-    //    cout << endl << endl;
+
     int temp;
     unordered_set<int> postOrd;
     list<int> ordered;
-    int max = 0;
+
     for(int i = 0; i < x; i++)
       {
 	cin >> temp;
-	postOrd.insert(temp);
-	ordered.push_back(temp);
-	//if(temp > max) max = temp;
+	postOrd.insert(temp); // used for linear contains
+	ordered.push_back(temp); // used to keep ordering of explicit gnomes
+
       }
     int count = 1;
-    while(x < y){
-      //      while( count < max){
-      while(!ordered.empty() && count >= ordered.front()){
-	//	  cout << "here" << endl;
+    while(x < y){ // only listing as many non explicit gnomes as allowed
+
+      while(!ordered.empty() && count >= ordered.front()){ // list explicit gnomes in correct spot
+
 	  cout << ordered.front() << endl;
 	  ordered.pop_front();
 	}
 
-      if(postOrd.find(count) == postOrd.end()){
+      if(postOrd.find(count) == postOrd.end()){ // list implicit gnomes as required
 	cout << count <<endl;
 	x++;
       }
       count++;
     }
-    while(!ordered.empty()){
+    while(!ordered.empty()){ // list remaining explicit gnomes
       cout << ordered.front() << endl;
       ordered.pop_front();
     }
