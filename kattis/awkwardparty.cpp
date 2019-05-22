04f57bcf04f4a33954f41750d52a2744b8f1d57f
jackschu <js9pa@virginia.edu>
2019-05-22 17:47:39 -0400

    removing unneeded edge case

--- kattis/awkwardparty.cpp
+++ kattis/awkwardparty.cpp
@@ -1,6 +1,5 @@
 #define DEBUG if(false)
 #include <iostream>
-#include <unordered_set>
 #include <unordered_map>
 #include <vector>
 
@@ -11,25 +10,16 @@ int main(){
   int n;
   cin >> n;
   vector<int> guests(n);
-  unordered_set<int> all_uniq;
-
   unordered_map<int, int> seen;  
-  all_uniq.reserve(n);
+  seen.reserve(n);  
   bool repeats = false;
   for(int i = 0; i < n; i++){
     int temp;
     cin >> temp;
     guests[i] = temp;
-    if (!repeats && all_uniq.count(temp) == 1){
-      repeats = true;
-    }
-    all_uniq.insert(temp);
   }
-  if (!repeats){
-    cout << n << endl;
-    return 0;
-  }
-  int max_min = 1E9;
+
+  int max_min = n;
   int global_offset = 0;
   for(auto elem : guests){
     DEBUG printf("elem %d\n", elem);
